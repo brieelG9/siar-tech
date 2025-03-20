8c367282b471a4968326a715f27c80e8433c10fa
Brieel <gabriel.silva1248@aluno.ce.gov.br>
2025-03-20 19:52:51 -0300

    Initial commit

---
+++ player/script/player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+@onready var anim : AnimatedSprite2D = $animacao
+
+func _physics_process(delta: float) -> void:
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	
+	var direction := Input.get_axis("esquerda", "direita")
+	if direction:
+		if direction > 0:
+			anim.scale.x = 5.0
+		if direction < 0:
+			anim.scale.x = -5.0
+			anim.play("idle")
+		velocity.x = direction * SPEED
+		anim.play("run")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
